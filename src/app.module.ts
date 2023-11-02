9fb99176cd5fb1872844b11820f17316094e93da
Douglas Crepes <douglascrepes@gmail.com>
2023-11-02 19:06:03 -0300

    Criado conexão MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    MongooseModule.forRoot('mongodb+srv://douglascrepes:K5Ul11cNfJyMntiF@cluster0.weh6krx.mongodb.net/?retryWrites=true&w=majority')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
