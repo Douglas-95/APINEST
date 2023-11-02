214e8da405bea2fc262acb98ffa9abed704f2786
Douglas Crepes <douglascrepes@gmail.com>
2023-11-02 19:25:07 -0300

    Configuração em Users Module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,13 @@
 import { Module } from "@nestjs/common";
 import { usersController } from "./users.controller";
 import { UserService } from "./users.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { User, UserSchema } from "./schemas/user.shema";
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
+    ],
     controllers: [usersController],
     providers: [UserService]
 })
