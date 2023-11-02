49e370c1ecc30d89ef8ee169e82d713f2d0e7f0c
Douglas Crepes <douglascrepes@gmail.com>
2023-11-02 19:41:05 -0300

    Configuração User Service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,26 @@
-import { Injectable } from "@nestjs/common";
-import { User } from "./schemas/user.shema";
-import { CreateUserDataDto } from "./dto/Create-User-Dto";
+import { ConflictException, Injectable } from "@nestjs/common";
+import { User, UserDocument } from "./schemas/user.shema";
+import { CreateUserDto } from "./dto/Create-User-Dto";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
 
 @Injectable()
 export class UserService{
-    private readonly users: User[] = [];
+    constructor(@InjectModel(User.name) private readonly userModel: Model<UserDocument>){}
+     
+    async createUser(createUserDto: CreateUserDto): Promise<User>{
+        const emailexists = await this.userModel.findOne({ email: createUserDto});
+
+        if(emailexists){
+            throw new ConflictException('Email informado já existe!');
+        }
+        const newUser = new this.userModel({
+            ...createUserDto
+        })
+        return await newUser.save()
+    }
     
-    async createUser(createUserDto: CreateUserDataDto): Promise<User>{
-        const newUser: User = {
-            id: Date.now().toString(),
-            ...createUserDto,
-        };
-        this.users.push(newUser);
-        return newUser
-    }
-    findAll(): User[]{
-        return this.users;
-    }
-    findOne(id: string): User{
-        return this.users.find(user => user.id === id);
+    async findAll(): Promise<User []>{
+        return await this.userModel.find().exec()
     }
 }
